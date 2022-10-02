6ef469c9c3e9c8fc00aad797662919e2cc6aa649
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-10-02 12:46:02 -0700

    Adding notepad steps with notes

---
+++ Tutorial_Classes/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_Classes
+{
+    internal class Course
+    {
+        // Fields
+        // Constructor
+        // Properties
+        // Methods
+
+    } // class
+
+} // namespace
